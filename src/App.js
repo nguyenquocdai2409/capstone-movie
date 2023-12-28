4e0c1ac994666ea306005dcab77ab206314ec192
nguyenquocdai2409 <nguyenquocdai2409@gmail.com>
2023-12-28 22:03:45 +0700

    update 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/Home/Home";
+import Login from "./page/Login/Login";
+import Layout from "./template/Layout";
+import InfoMovie from "./page/InfoMovie/InfoMovie";
+import Register from "./page/Register/Register";
+import BookTicket from "./page/BookTicket/BookTicket";
+import Loading from "./components/Loading";
+import UserInfo from "./page/UserInfo/UserInfo";
+import PageNotFound from "./page/Page_404/PageNotFound";
+import "moment/locale/vi";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Loading />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path='/detail/:id'
+            element={
+              <Layout>
+                <InfoMovie />
+              </Layout>
+            }
+          />
+          <Route
+            path='/login'
+            element={
+              <Layout>
+                <Login />
+              </Layout>
+            }
+          />
+          <Route
+            path='/register'
+            element={
+              <Layout>
+                <Register />
+              </Layout>
+            }
+          />
+          <Route
+            path='/ticketroom/:id'
+            element={
+              <Layout>
+                <BookTicket />
+              </Layout>
+            }
+          />
+          <Route
+            path='/profile'
+            element={
+              <Layout>
+                <UserInfo />
+              </Layout>
+            }
+          />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
